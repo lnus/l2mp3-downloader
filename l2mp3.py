972838d0849e6d4d688364b93a42f7d3aa18dcdf
lnus_ <linuzlp@gmail.com>
2017-01-27 09:31:35 +0100

    Add files via upload

--- l2mp3.py
+++ l2mp3.py
@@ -1,19 +1,14 @@
 import os, time
 from colorama import Fore
 from pytube import YouTube
-import moviepy.editor as mp
 
 class Downloader():
     def __init__(self):
         # Makes the dirs
         if not os.path.exists("Video"):
-            os.mkdir("Video")
+            os.system("Video")
         if not os.path.exists("Audio"):
-            os.mkdir("Audio")
-
-        # Args
-        self.video_only = False
-        self.audio_only = False
+            os.system("mkdir Audio")
 
         # Mainloop
         self.start_search()
@@ -22,7 +17,7 @@ class Downloader():
         # Settings
         yt = YouTube(url)
         print("Found video: " + yt.filename)
-        filename = yt.filename.replace("!", "")
+        filename = yt.filename.replace("!", "").replace("&","").replace(" ","-")
 
         # Finds the correct resolution
         res_info = yt.filter("mp4")[-1]
@@ -33,18 +28,14 @@ class Downloader():
         video = yt.get("mp4", res)
         
         # Downloads the video in the correct resolution
-        video.download("Video/")
+        video.download("Video/"+filename+".mp4")
         
         # Gets the audio
         self.audio_convert(filename)
 
     def audio_convert(self, filename):
-        # Converts a video to an mp3 file
-        clip = mp.VideoFileClip("Video/" + filename + ".mp4")
-        clip.audio.write_audiofile("Audio/" + filename + ".mp3")
-
-        # Flushes the clip variable
-        clip = None
+        # Converts a video to an mp3 file using FFmpeg
+        os.system("ffmpeg -i {} {}".format("Video/"+filename+".mp4", "Audio/"+filename+".mp3"))
 
     def start_search(self):        
         while True:
